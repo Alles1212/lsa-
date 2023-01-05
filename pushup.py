6f7a357c2fba0aaf8ee4521b309a4d18d05f0c45
Alles <82037602+Alles1212@users.noreply.github.com>
2023-01-05 15:10:38 +0800

    Update pushup.py

--- pushup.py
+++ pushup.py
@@ -1,7 +1,7 @@
 import cv2
 from PoseModule import PoseModule
 import os
-import cowsay #可在Linux下執行，會有驚喜(記得先sudo pip install cowsay)
+import cowsay #可以拿掉這行、可在Linux下執行，會有驚喜(記得先sudo pip install cowsay)
 import random
 
 
